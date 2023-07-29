ebce0f83e1d5557a32795ddee3788a7d1f574e17
Bimsara Walallawita <bwalallawita@gmail.com>
2023-07-29 13:54:05 +0530

    JWT Token Authentication

---
+++ src/main/java/com/example/jwt/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.jwt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.jwt.entity.User;
+
+
+public interface UserRepository extends JpaRepository<User , Long>{
+    //Custom queries
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
+}
